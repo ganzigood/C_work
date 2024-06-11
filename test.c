59528233807abe4faf35c8ae98d52f96078b07e2
good4ram@naver.com <good4ram@naver.com>
2024-06-11 21:13:05 +0900

    start

--- test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "test.h"
-
-int sum(num a) {
-
-    return a+10;
-}
--- test.h
+++
@@ -1,8 +0,0 @@
-#ifndef __test_h__
-#define __test_h__
-
-typedef int num;
-
-int sum(num a);
-
-#endif
